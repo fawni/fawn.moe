e404ad47dfe4a6307b55531a25483aa77360b462
fawn <fawn@rinici.de>
2024-04-17 05:34:30 +0200

    ✨ feature: init

---
+++ src/scripts/global.ts
@@ -0,0 +1,5 @@
+console.log(
+  "%c ♡ %c it's dangerous to go alone! take this.",
+  "background-color: #eb99a1; color: white; font-weight: bold; border-radius: 8px;",
+  "font-style: italic; color: white",
+);
