36bd6adc68d64e97f3ab62a1cce2caeef4ef12aa
fawn <fawn@rinici.de>
2024-04-17 19:56:44 +0200

    ✨ feature: toggleable pronouns, selection css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
 import inline from "@playform/inline";
 import compress from "astro-compress";
 import robots from "astro-robots-txt";
+
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
