bdfcfb50229471531c6ffec76c9fc97308f00d20
fawn <fawn@rinici.de>
2024-04-17 11:17:07 +0200

    ♻️ refactor: preload assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 import inline from "@playform/inline";
+import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
