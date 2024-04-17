891996511430826b6a139e65df1d9610eebf456b
fawn <fawn@rinici.de>
2024-04-17 10:24:35 +0200

    ✨ feature: add sitemap and compress integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
+import playformCompress from "@playform/compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap(), playformCompress()],
 });
