1a4f9424f6f09c1c32dbc827788e0a9e2a483738
fawn <fawn@rinici.de>
2024-07-28 17:01:29 +0300

    feat: add polybar-like header (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import inline from "@playform/inline";
-import compress from "astro-compress";
 import robots from "astro-robots-txt";
-
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fawn.moe",
-  integrations: [mdx(), sitemap(), robots(), inline(), compress()],
+  integrations: [svelte(), mdx(), sitemap(), robots(), inline(), (await import("@playform/compress")).default({Image: false})],
 });
+
