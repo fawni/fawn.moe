7c329b638d670a4951bb197ec01be9f8b729fa91
fawn <fawn@rinici.de>
2024-07-28 17:13:54 +0300

    fix: absolute (kinda) background path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
+import compress from "@playform/compress";
 import inline from "@playform/inline";
 import robots from "astro-robots-txt";
 import { defineConfig } from "astro/config";
@@ -8,6 +9,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://fawn.moe",
-  integrations: [svelte(), mdx(), sitemap(), robots(), inline(), (await import("@playform/compress")).default({Image: false})],
+  integrations: [svelte(), mdx(), sitemap(), robots(), inline(), compress()],
 });
-
