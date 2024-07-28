786d76654191386f17eaef729dd42620b9de7b29
fawn <fawn@rinici.de>
2024-07-28 16:59:48 +0300

    fix: re-disable sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import playformCompress from "@playform/compress";
 import inline from "@playform/inline";
 import robots from "astro-robots-txt";
 import { defineConfig } from "astro/config";
@@ -9,6 +8,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://fawn.moe",
-  integrations: [svelte(), mdx(), sitemap(), robots(), inline(), playformCompress()],
+  integrations: [svelte(), mdx(), sitemap(), robots(), inline(), (await import("@playform/compress")).default({Image: false})],
 });
 
