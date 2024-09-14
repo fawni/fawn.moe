0c335c67b3fe2b612ce102cef892b9d21a659a79
fawn <fawn@rinici.de>
2024-09-14 17:25:34 +0300

    init

--- src/scripts/global.ts
+++
@@ -1,5 +0,0 @@
-console.log(
-  "%c ♡ %c it's dangerous to go alone! take this.",
-  "background-color: #eb99a1; color: white; font-weight: bold; border-radius: 8px;",
-  "font-style: italic; color: white",
-);
